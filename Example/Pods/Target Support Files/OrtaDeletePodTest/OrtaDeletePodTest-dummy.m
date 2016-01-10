9a35e7b0bd3ff1cb0b6841a7750b7626a2fdc34a
Orta Therox <orta.therox@gmail.com>
2016-01-10 11:16:14 +0000

    Init

---
+++ Example/Pods/Target Support Files/OrtaDeletePodTest/OrtaDeletePodTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_OrtaDeletePodTest : NSObject
+@end
+@implementation PodsDummy_OrtaDeletePodTest
+@end
---
+++ Example/Pods/Target Support Files/OrtaDeletePodTest/OrtaDeletePodTest-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double OrtaDeletePodTestVersionNumber;
+FOUNDATION_EXPORT const unsigned char OrtaDeletePodTestVersionString[];
+
